e3092f64f0eb1aab37a77d93b9c10faabfa3f1a0
sozenarede <sozenarede@gmail.com>
2022-08-21 17:49:56 -0300

    Commit inicial com projeto angular + spring boot

---
+++ src/main/kotlin/sozenarede/casa/angularspring/AngularSpringApplication.kt
@@ -0,0 +1,11 @@
+package sozenarede.casa.angularspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AngularSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<AngularSpringApplication>(*args)
+}
---
+++ src/test/kotlin/sozenarede/casa/angularspring/AngularSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package sozenarede.casa.angularspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AngularSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
