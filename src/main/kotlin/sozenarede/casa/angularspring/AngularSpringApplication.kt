f211512fdc90e2f8624a4234e5597511537695eb
sozenarede <sozenarede@gmail.com>
2022-08-21 18:20:40 -0300

    Adding bootstrap CSS + api spring

--- src/main/kotlin/sozenarede/casa/angularspring/AngularSpringApplication.kt
+++ src/main/kotlin/sozenarede/casa/angularspring/AngularSpringApplication.kt
@@ -2,9 +2,24 @@ package sozenarede.casa.angularspring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
+
 
 @SpringBootApplication
-class AngularSpringApplication
+@Controller
+class AngularSpringApplication {
+  @GetMapping("/resource")
+  @ResponseBody
+  fun home(): Map<String, Any>? {
+    val model: MutableMap<String, Any> = HashMap()
+    model["id"] = UUID.randomUUID().toString()
+    model["content"] = "Hello World"
+    return model
+  }
+}
 
 fun main(args: Array<String>) {
 	runApplication<AngularSpringApplication>(*args)
